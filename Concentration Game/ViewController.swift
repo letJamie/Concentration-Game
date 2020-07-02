27cdedafd80f9f7804d3949a411280a8dd579da0
letJamie <stillbut@naver.com>
2020-07-02 21:25:32 +0900

    Add flip function

--- Concentration Game/ViewController.swift
+++ Concentration Game/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        
+        FlipCard(name: "👻", button: sender)
+        
     }
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        
+        FlipCard(name: "🎃", button: sender)
 
-
+        
+    }
+    func FlipCard(name: String, button: UIButton) {
+        if button.currentTitle == name {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        } else {
+            button.setTitle(name, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.2952712513, green: 0.6914016126, blue: 1, alpha: 1)
+        }
+    }
 }
 
