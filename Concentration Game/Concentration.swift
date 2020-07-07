6a3fd030487b03c2f537a1d8a0e2678f90f0485a
letJamie <stillbut@naver.com>
2020-07-07 23:53:23 +0900

    Add auto layout

--- Concentration Game/Concentration.swift
+++ Concentration Game/Concentration.swift
@@ -9,17 +9,31 @@
 import Foundation
 
 class Concentration {
-    var cards = [Card]() 
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
     
     func chooseCard(at index: Int) {
         
-        if cards[index].isFaceUp {
-            //print(cards[index].isFaceUp)
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if card match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                //no card or two cards are faced up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
-        
+         
     }
     
     init(numberOfPairsOfCards: Int ) {
